38d8e854cd7fab24460cc02cb531aad3c116c3a5
imjsw <65204023+imjsw@users.noreply.github.com>
2024-11-16 22:57:11 +0800

    Add files via upload

--- rootlogin.sh
+++ rootlogin.sh
@@ -2,7 +2,7 @@
 
 # 设置 ROOT 密码
 echo "设置你的ROOT密码"
-echo -e "your_new_password\rooT123@ | sudo passwd root
+echo -e "your_new_password\nyour_new_password" | sudo passwd root
 
 # 启用 root 登录和密码认证
 sudo sed -i 's/^\s*#\?\s*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
@@ -16,5 +16,3 @@ sudo systemctl restart sshd
 
 # 完成提示
 echo -e "ROOT登录设置完毕"
-
-
